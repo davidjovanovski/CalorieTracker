5a7e8ef5d4c7e35169e8c992f8e0adb22abe65d4
David Jovanovski <dasejovanovski@live.com>
2026-04-30 16:51:43 +0200

    Opravanje path

---
+++ CalorieTracker/src/main/java/personal/calorietracker/Models/User.java
@@ -0,0 +1,20 @@
+package personal.calorietracker.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nameSurname;
+    int weight, height;
+}
