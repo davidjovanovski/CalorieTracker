c6c2a732a899c373cec073fcc8828b929cd7e1d8
David Jovanovski <dasejovanovski@live.com>
2026-04-30 16:12:31 +0200

    Initial Commit 2

---
+++ CalorieTracker/src/main/java/personal/calorietracker/CalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package personal.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieTrackerApplication.class, args);
+    }
+
+}
